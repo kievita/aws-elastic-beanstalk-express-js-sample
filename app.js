e8a143e434ebbdcccf51ce9bf69a00a4ac23ec29
Martinez Escobar <alejandro.martinez-escobar@capgemini.com>
2021-02-11 16:30:04 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola caracola'));
+app.get('/', (req, res) => res.send('Este es el cambio del Review en la Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
